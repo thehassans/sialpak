0644ce84f079ec02211cb50ee62fbca15771ad6c
thehassans <thehassans@github.com>
2026-07-11 09:55:21 +0300

    chore: add explicit crash logging to server.js for debugging Passenger

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const fs = require('fs')
+process.on('uncaughtException', (err) => {
+  fs.appendFileSync('crash.log', new Date().toISOString() + ' Uncaught Exception: ' + err.stack + '\n')
+})
+process.on('unhandledRejection', (reason, promise) => {
+  fs.appendFileSync('crash.log', new Date().toISOString() + ' Unhandled Rejection: ' + reason + '\n')
+})
+const originalConsoleError = console.error;
+console.error = function (...args) {
+  fs.appendFileSync('crash.log', new Date().toISOString() + ' console.error: ' + args.join(' ') + '\n');
+  originalConsoleError.apply(console, args);
+}
+
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
