cc9118d053dceba23bd6fe228309da86a0be12b8
thehassans <thehassans@github.com>
2026-07-11 10:08:05 +0300

    fix: disable Next.js Image Optimization to allow direct remote image loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
