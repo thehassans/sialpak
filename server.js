563d7e99b00e6af2bb28d2b743fbb4f0f6d812d2
thehassans <thehassans@github.com>
2026-07-11 09:49:38 +0300

    revert: remove unstable in-memory mongodb, restore sqlite

--- server.js
+++ server.js
@@ -1,31 +1,17 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
-const { MongoMemoryReplSet } = require('mongodb-memory-server')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-MongoMemoryReplSet.create({ replSet: { count: 1 } }).then((replSet) => {
-  const uri = replSet.getUri('buysial');
-  process.env.DATABASE_URL = uri;
-  console.log("Memory Mongo Started at:", uri);
-  
-  // We push schema quickly before starting
-  console.log("Pushing schema to Memory Mongo...");
-  require('child_process').execSync('npx prisma db push --accept-data-loss', { stdio: 'inherit' });
-
-  app.prepare().then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true)
-      handle(req, res, parsedUrl)
-    }).listen(process.env.PORT || 3000, (err) => {
-      if (err) throw err
-      console.log('> Ready on http://localhost:' + (process.env.PORT || 3000))
-    })
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000))
   })
-}).catch(err => {
-  console.error("Failed to start MongoMemoryReplSet:", err);
-  process.exit(1);
 })
